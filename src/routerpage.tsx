84240653704009d7f8d2d5092fe3ef5c7afb0323
Salih Ertuğrul ER <141808973+ertugrulsaliher@users.noreply.github.com>
2024-08-06 16:22:01 +0300

    anasayfa

--- src/routerpage.tsx
+++ src/routerpage.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
+
 import PendingUsers from "./pages/admin/PendingUsers"
+import VisitorPage from "./pages/visitorPage/VisitorPage";
+import UserStories from "./pages/userStories/UserStories";
+import Resources from "./pages/resources/Resources";
 
 function RouterPage() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<></>}/>
+        <Route path="/" element={<VisitorPage/>}/>
+        <Route path="/userstories" element={<UserStories/>}/>
+        <Route path="/resources" element={<Resources/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/PendingUsers" element={<PendingUsers />} />
